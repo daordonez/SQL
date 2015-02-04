fe4c842865088f1113347b0ea420fb44ba6b8381
Diego Ordóñez Morales <diegordonez4@gmail.com>
2015-02-05 00:46:04 +0100

    Inserts
    Equipos
    Colores maillot
    Equipo AGR2 Mondiale
    BMC Racing

---
+++ bd ciclistas/INSERTS.sql
@@ -0,0 +1,144 @@
+#---------------------------
+# Equipos ciclistas
+#---------------------------
+INSERT INTO equipos VALUES("Agr2 Mondiale","Vincent Lavenu");
+INSERT INTO equipos VALUES("BMC Racing","James Ochowiz");
+INSERT INTO equipos VALUES("Caja Rural","Jose Manuel Hernandez");
+INSERT INTO equipos VALUES("Cofidis le credit en ligne","Yvon Sanquer");
+INSERT INTO equipos VALUES("FDJ","Marc Madiot");
+INSERT INTO equipos VALUES("Garmin-Cervelo","Jonathan Vaughters");
+INSERT INTO equipos VALUES("Greenedege Cycling Team","Shayne Bannan");
+INSERT INTO equipos VALUES("Katusha Teams","Viacheslav Ekimov");
+INSERT INTO equipos VALUES("Lampere-ISD","Giuseppe Saronni");
+INSERT INTO equipos VALUES("Leopard Trek","Luca Guercilena");
+INSERT INTO equipos VALUES("Liquigas-Canondale","Roberto Amadio");
+INSERT INTO equipos VALUES("Movistar Team","Eusebio Unzué");
+INSERT INTO equipos VALUES("Mtn-Qhubeka","Tiann Kannemeyer");
+INSERT INTO equipos VALUES("Omega Pharma-Lotto","Patrick Lefevere");
+INSERT INTO equipos VALUES("Pro Team Astana","Aleksandr Vinokurov");
+INSERT INTO equipos VALUES("RaboBank","Richard Plugge");
+INSERT INTO equipos VALUES("Saxo Bank Sunguard","Bjarme Riis");
+INSERT INTO equipos VALUES("Team Giant-Alpecin","Iwan Spekenbrink");
+INSERT INTO equipos VALUES("Sky Pro cycling","Dave Brailsfor");
+INSERT INTO equipos VALUES("Team Europcar","Jean-René Bernaudeau");
+INSERT INTO equipos VALUES("Team Radioshack","Bill Stapleton");
+
+#---------------------
+# Colores maillot
+#---------------------
+INSERT INTO maillot(tipo,color,premio) VALUES("Lider","Rojo",8000);
+INSERT INTO maillot(tipo,color,premio) VALUES("G_Montaña","Naranja",4000);
+INSERT INTO maillot(tipo,color,premio) VALUES("Combinado","Blanco",3000);
+INSERT INTO maillot(tipo,color,premio) VALUES("Ex_Puntos","Azul",2000);
+INSERT INTO maillot(tipo,color,premio) VALUES("Campeon","Amarillo",350000);
+
+#----------------------------------
+# Equipo Agr2 Mondiale
+#------------------------------------
+
+INSERT INTO ciclistas VALUES(11,"Huber Dupont",34,"Agr2 Mondiale");
+INSERT INTO ciclistas VALUES(12,"Carlos Alberto Betancur",25,"Agr2 Mondiale");
+INSERT INTO ciclistas VALUES(13,"Maxime Bouet",28,"Agr2 Mondiale");
+INSERT INTO ciclistas VALUES(14,"Damien Gaudin",28,"Agr2 Mondiale");
+INSERT INTO ciclistas VALUES(15,"Patrick Getsch",27,"Agr2 Mondiale");
+INSERT INTO ciclistas VALUES(16,"Yauheni Hutarovich",32,"Agr2 Mondiale");
+INSERT INTO ciclistas VALUES(17,"Lloyd Mondory",32,"Agr2 Mondiale");
+INSERT INTO ciclistas VALUES(18,"Rinaldo Nocentini",37,"Agr2 Mondiale");
+INSERT INTO ciclistas VALUES(19,"Sebastien Turgot",30,"Agr2 Mondiale");
+
+#-------------------------
+# Equipo BMC Racing
+#--------------------------
+INSERT INTO ciclistas VALUES(41,"Samuel Sanchez Gonzalez",36,"BMC Racing");
+INSERT INTO ciclistas VALUES(42,"Rohan Dennis",25,"BMC Racing");
+INSERT INTO ciclistas VALUES(43,"Cadel Evans",38,"BMC Racing");
+INSERT INTO ciclistas VALUES(44,"Philippe Gilbert",33,"BMC Racing");
+INSERT INTO ciclistas VALUES(45,"Steve Morabito",32,"BMC Racing");
+INSERT INTO ciclistas VALUES(46,"Dominik Nerz",26,"BMC Racing");
+INSERT INTO ciclistas VALUES(47,"Manuel Quinzquiato",36,"BMC Racing");
+INSERT INTO ciclistas VALUES(48,"Larry Warbasse",25,"BMC Racing");
+INSERT INTO ciclistas VALUES(49,"Danilo Wyss",29,"BMC Racing");
+#-------------------------
+# Equipo Caja Rural
+#-------------------------
+INSERT INTO ciclistas VALUES(,"",,"Caja Rural");
+INSERT INTO ciclistas VALUES(,"",,"Caja Rural");
+INSERT INTO ciclistas VALUES(,"",,"Caja Rural");
+INSERT INTO ciclistas VALUES(,"",,"Caja Rural");
+INSERT INTO ciclistas VALUES(,"",,"Caja Rural");
+INSERT INTO ciclistas VALUES(,"",,"Caja Rural");
+INSERT INTO ciclistas VALUES(,"",,"Caja Rural");
+INSERT INTO ciclistas VALUES(,"",,"Caja Rural");
+INSERT INTO ciclistas VALUES(,"",,"Caja Rural");
+#-------------------------
+# Equipo Cofidis le credit
+#-------------------------
+INSERT INTO ciclistas VALUES(,"",,"Cofidis le credit en");
+INSERT INTO ciclistas VALUES(,"",,"Cofidis le credit en");
+INSERT INTO ciclistas VALUES(,"",,"Cofidis le credit en");
+INSERT INTO ciclistas VALUES(,"",,"Cofidis le credit en");
+INSERT INTO ciclistas VALUES(,"",,"Cofidis le credit en");
+INSERT INTO ciclistas VALUES(,"",,"Cofidis le credit en");
+INSERT INTO ciclistas VALUES(,"",,"Cofidis le credit en");
+INSERT INTO ciclistas VALUES(,"",,"Cofidis le credit en");
+INSERT INTO ciclistas VALUES(,"",,"Cofidis le credit en");
+#-------------------------
+# Equipo FDJ
+#-------------------------
+INSERT INTO ciclistas VALUES(,"",,"FDJ");
+INSERT INTO ciclistas VALUES(,"",,"FDJ");
+INSERT INTO ciclistas VALUES(,"",,"FDJ");
+INSERT INTO ciclistas VALUES(,"",,"FDJ");
+INSERT INTO ciclistas VALUES(,"",,"FDJ");
+INSERT INTO ciclistas VALUES(,"",,"FDJ");
+INSERT INTO ciclistas VALUES(,"",,"FDJ");
+INSERT INTO ciclistas VALUES(,"",,"FDJ");
+INSERT INTO ciclistas VALUES(,"",,"FDJ");
+#-------------------------
+# Equipo Garmin - Cervelo
+#-------------------------
+#-------------------------
+# Equipo Greenedege
+#-------------------------
+#-------------------------
+# Equipo Katusha
+#-------------------------
+#-------------------------
+# Equipo Lampere
+#-------------------------
+#-------------------------
+# Equipo Liquigas
+#-------------------------
+#-------------------------
+# Equipo Movistar Team
+#-------------------------
+#-------------------------
+# Equipo NTN Qhubeka
+#-------------------------
+#-------------------------
+# Equipo Omega Pharma
+#-------------------------
+#-------------------------
+# Equipo Pro Team Astana
+#-------------------------
+#-------------------------
+# Equipo Quickstep
+#-------------------------
+#-------------------------
+# Equipo RaboBank
+#-------------------------
+#-------------------------
+# Equipo 
+#-------------------------
+#-------------------------
+# Equipo Saxo Bank
+#-------------------------
+#-------------------------
+# Equipo SKY Pro Cycling
+#-------------------------
+#-------------------------
+# Equipo Team Europcar
+#-------------------------
+#-------------------------
+# Equipo Radioshack
+#-------------------------
